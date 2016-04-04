ac405b97506ce1455a2ecc097f5575b0e62c87d4
David Prichard <davidprichard64@gmail.com>
2016-04-04 14:13:30 -0400

    Added project skeleton
    Added the main object (entry point) and other scala classes we will
    probably need. It’s rough, but it’s a start.

---
+++ src/term/project/SentimentAnalysis.scala
@@ -0,0 +1,9 @@
+package term.project
+
+/**
+  * Methods and classes for parsing textual data to determine sentiment.
+  */
+
+class SentimentAnalysis {
+
+}
---
+++ src/term/project/WebCrawler.scala
@@ -0,0 +1,16 @@
+package term.project
+
+/**
+  * Methods and classes for crawling the web or otherwise obtaining data.
+  */
+
+class WebCrawler {
+  // collectData includes domain/page discovery, data scraping, and data sanitizing.
+  //def collectData(sport: String, league: String, team: String): SanitaryData = {
+
+  //initial discovery
+
+  //scrape data
+
+  //sanitize data
+}
