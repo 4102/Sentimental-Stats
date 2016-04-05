c4b21e1d32d211956d45d7c571a6ab0a0ff6570f
David Prichard <davidprichard64@gmail.com>
2016-04-05 18:37:32 -0400

    So many changes
    Sooooooo much

--- src/term/project/Main.scala
+++ src/term/project/Main.scala
@@ -4,7 +4,7 @@ package term.project
   * The entry point and main object that executes the program.
   */
 
-/* Extending the App trait is a common way to define an entry point in Scala.
+/* Extending the App trait is the idiomatic way to define an entry point in Scala.
  *
  * The statements in the body of an object extending App will be executed in order,
  * similar to a 'main' method in Java, and also makes any command-line arguments
@@ -16,20 +16,13 @@ object Main extends App {
   println("Sentimental Stats")
   println("Arguments: " + (args mkString ", "))
 
-  /*
-  for {
-    sport <- sports
-    league <- sport
-    team <- league
-  } yield {
-    new Tuple3(sport, league, team);
-  }
-  */
+  println("Team name" + args(0))
+  println("Team home" + args(1))
 
-  // val crawler = new WebCrawler
-  // crawler.collectData(sport : String, league : String, team : String) : data
+  val teamName = args(0)
+  val teamHome = args(1)
 
-  // filterData(data)       // escaping, filtering, deduplication etc.
+  val results : Array[String] = WebCrawler.search(teamName, teamHome)
 
   // logData(data)          // write to log file for debugging
 
---
+++ src/term/project/Utils.scala
@@ -0,0 +1,8 @@
+package term.project
+
+/**
+  * A singleton object to hold useful functions/static methods, like reading from a config file.
+  */
+object Utils {
+
+}
